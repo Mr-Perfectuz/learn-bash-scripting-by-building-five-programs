c6bc9aa258a6bc8d4999a3588ed3e5a2d1533d82
Anvarbek Ziyodov <ziyodovanvar1999@gmail.com>
2024-11-21 11:37:39 +0000

    Save progress: 1425.1

--- fortune.sh
+++ fortune.sh
@@ -6,33 +6,24 @@ echo -e "\n~~ Fortune Teller ~~\n"
 # Declare the RESPONSES array
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 
-# Display the RESPONSES array (Optional, for debugging)
-# echo "${RESPONSES[@]}"
-
-# Generate a random index to select a fortune
-RANDOM_INDEX=$((RANDOM % ${#RESPONSES[@]}))
-FORTUNE=${RESPONSES[$RANDOM_INDEX]}
-
-# Display the fortune
-echo "Your fortune: $FORTUNE"
- echo ${RESPONSES[$N]}
-
-N=$(( RANDOM % 6 ))
-
+# Function to get a fortune
 function GET_FORTUNE() {
-if [[ ! $1 ]]
-then
-  echo Ask a yes or no question:
-else
-  echo Try again. Make sure it ends with a question mark:
-fi
-
+  if [[ ! $1 ]]; then
+    echo "Ask a yes or no question:"
+  else
+    echo "Try again. Make sure it ends with a question mark:"
+  fi
   read QUESTION
 }
 
+# Loop until a question ending with '?' is provided
 until [[ $QUESTION =~ \?$ ]]
 do
-GET_FORTUNE
-  GET_FORTUNE again
-  
+  GET_FORTUNE "again"
 done
+
+# Generate a random index to select a fortune
+N=$((RANDOM % ${#RESPONSES[@]}))
+
+# Display the fortune with a newline
+echo -e "\n${RESPONSES[$N]}"
