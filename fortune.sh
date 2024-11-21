cfe3d0fe16c981890445fae923c73fb725ddd8ea
Anvarbek Ziyodov <ziyodovanvar1999@gmail.com>
2024-11-21 11:32:59 +0000

    update

---
+++ fortune.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Program to tell a person's fortune
+
+echo -e "\n~~ Fortune Teller ~~\n"
+
+# Declare the RESPONSES array
+RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+
+# Display the RESPONSES array (Optional, for debugging)
+# echo "${RESPONSES[@]}"
+
+# Generate a random index to select a fortune
+RANDOM_INDEX=$((RANDOM % ${#RESPONSES[@]}))
+FORTUNE=${RESPONSES[$RANDOM_INDEX]}
+
+# Display the fortune
+echo "Your fortune: $FORTUNE"
+ echo ${RESPONSES[$N]}
+
+N=$(( RANDOM % 6 ))
+GET_FORTUNE() {
+  echo Ask a yes or no question:
+  read QUESTION
+}
+
+until [[ $QUESTION == test? ]]
+do
+  GET_FORTUNE
+done
