19d343483718e97a3e8e021de1b2c9b73dc790b7
Anvarbek Ziyodov <ziyodovanvar1999@gmail.com>
2024-11-21 11:29:45 +0000

    Save progress: 1220.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -15,9 +15,13 @@ FORTUNE=${RESPONSES[$RANDOM_INDEX]}
 
 # Display the fortune
 echo "Your fortune: $FORTUNE"
-echo ${RESPONSES[$N]}
+ echo ${RESPONSES[$N]}
+
 N=$(( RANDOM % 6 ))
 GET_FORTUNE() {
   echo Ask a yes or no question:
+  read QUESTION
 }
+
 GET_FORTUNE
+echo $QUESTION
