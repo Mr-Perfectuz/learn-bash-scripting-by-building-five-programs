6dd74712f52c37bb1f75373e86e19abae2806b3e
Anvarbek Ziyodov <ziyodovanvar1999@gmail.com>
2024-11-21 11:25:01 +0000

    Save progress: 1110.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-#Bingo Number Generator 
-echo -e "\n~~ Bingo Number Generator ~~\n"
-NUMBER=$(( RANDOM % 15+61 ))
-TEXT="The next number is, "
-if (( NUMBER <= 15 ))
-then
-  echo $TEXT B:$NUMBER
-elif [[ $NUMBER -le 30 ]]
-then
-  echo $TEXT I:$NUMBER
-elif (( NUMBER < 46 ))
-then
-  echo $TEXT N:$NUMBER
-elif [[ $NUMBER -lt 61 ]]
-then
-  echo $TEXT G:$NUMBER
-else
-  echo $TEXT O:$NUMBER
-fi
+# Program to tell a person's fortune
+
+echo -e "\n~~ Fortune Teller ~~\n"
+
+# Declare the RESPONSES array
+RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+
+# Display the RESPONSES array (Optional, for debugging)
+# echo "${RESPONSES[@]}"
+
+# Generate a random index to select a fortune
+RANDOM_INDEX=$((RANDOM % ${#RESPONSES[@]}))
+FORTUNE=${RESPONSES[$RANDOM_INDEX]}
+
+# Display the fortune
+echo "Your fortune: $FORTUNE"
+declare -p RESPONSES
